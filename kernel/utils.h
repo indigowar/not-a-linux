556ed8e6251d198bf408b6cb4dbc7bc7994ea82a
Indigowar <indigowar.github@gmail.com>
2022-07-08 13:17:04 +0300

    remove old barebones

--- kernel/utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef H_NAL_KERNEL_UTILS_H
-#define H_NAL_KERNEL_UTILS_H
-
-// copy memory from (0) to (1) and count of (2)
-void memncpy(char*, char*, int);
-
-void int_to_ascii(int n, char str[]);
-
-#endif  // H_NAL_KERNEL_UTILS_H
